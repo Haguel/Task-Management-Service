ed669e692bc5767c0afaaa0c679b4a68dabdebb3
GlebHal <hammanov.gleb@gmail.com>
2024-06-18 09:48:54 +0300

    Init backend

---
+++ backend/src/test/java/com/task_management_service/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task_management_service.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
