582ca2ed9b3a5cc5a53dd1b00e68b6bbea668544
Alexandr Kalyan <110027812+quk1se@users.noreply.github.com>
2024-06-21 14:36:53 +0300

    mainpage

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
-import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainpageComponent,
-    CarouselComponent
   ],
   imports: [
     BrowserModule,
