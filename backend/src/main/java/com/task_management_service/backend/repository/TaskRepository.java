7f465ccdbe6a36da04646f41862f2dad114ce6d4
GlebHal <hammanov.gleb@gmail.com>
2024-06-22 11:18:42 +0300

    Added task scheduler
    - Task scheduler checks whether task expired and make its status EXPIRED if so
    - Added socket configuration (if task expired, new list of tasks will be send to frontend)

--- backend/src/main/java/com/task_management_service/backend/repository/TaskRepository.java
+++ backend/src/main/java/com/task_management_service/backend/repository/TaskRepository.java
@@ -1,11 +1,15 @@
 package com.task_management_service.backend.repository;
 
 import com.task_management_service.backend.entity.TaskEntity;
+import com.task_management_service.backend.enumeration.TaskStatus;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends CrudRepository<TaskEntity, UUID> {
+    List<TaskEntity> findByUntilDateBeforeAndStatusNot(LocalDateTime untilDate, TaskStatus status);
 }
