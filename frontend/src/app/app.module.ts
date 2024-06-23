42eed78cefa9479774e0e2d6b2329e7b3028e9de
Alexandr Kalyan <110027812+quk1se@users.noreply.github.com>
2024-06-23 13:26:04 +0300

    registration validation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { RegistrationformComponent } from './registrationform/registrationform.component';
 import { LoginformComponent } from './loginform/loginform.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
--- frontend/src/app/registrationform/registrationform.component.ts
+++ frontend/src/app/registrationform/registrationform.component.ts
@@ -6,11 +6,36 @@ import { Component } from '@angular/core';
   styleUrl: './registrationform.component.css'
 })
 export class RegistrationformComponent {
-  username: string = ''
-  password: string = ''
-  email: string = ''
+  username: string = '';
+  password: string = '';
+  email: string = '';
+  isValidEmail: boolean = true;
+  isValidPassword: boolean = true;
+  isValidUsername: boolean = true;
 
-  myClick() {
-    alert("hello world")
+
+
+  checkEmailValid() {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    this.isValidEmail = emailPattern.test(this.email);
+  }
+
+  checkPasswordValid() {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
+    this.isValidPassword = passwordRegex.test(this.password);
+  }
+
+  checkUsernameValid() {
+    if (this.username.length < 5) this.isValidUsername = false;
+    else this.isValidUsername = true;
+  }
+
+  signUpClick(): void {
+    if (this.username == '' || this.password == '' || this.email == '') {
+      this.isValidUsername = false
+      this.isValidPassword = false
+      this.isValidEmail = false
+      alert('Please enter a valid data');
+    }
   }
 }
