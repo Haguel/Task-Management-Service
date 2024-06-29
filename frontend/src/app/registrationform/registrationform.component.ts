7201a2f5c1eb6378d5fe89191265c28e2655ebfc
quk1se <alexandrkalyan953@gmail.com>
2024-06-29 08:02:12 +0300

    Dashboard and authentication fixes

--- frontend/src/app/registrationform/registrationform.component.ts
+++ frontend/src/app/registrationform/registrationform.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-registrationform',
@@ -6,14 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './registrationform.component.css'
 })
 export class RegistrationformComponent {
+  name: string = '';
   username: string = '';
   password: string = '';
   email: string = '';
+
   isValidEmail: boolean = true;
   isValidPassword: boolean = true;
   isValidUsername: boolean = true;
+  isValidName: boolean = true;
+  isValidForm: boolean = true;
 
+  registrationResponse: any;
+  token: string = '';
 
+  constructor(private authService: AuthService) { }
 
   checkEmailValid() {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -21,21 +30,55 @@ export class RegistrationformComponent {
   }
 
   checkPasswordValid() {
-    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,50}$/;
     this.isValidPassword = passwordRegex.test(this.password);
   }
 
   checkUsernameValid() {
-    if (this.username.length < 5) this.isValidUsername = false;
-    else this.isValidUsername = true;
+    const usernameRegex = /^[a-zA-Z0-9]{4,20}$/;
+    this.isValidUsername = usernameRegex.test(this.username);
+  }
+
+  checkNameValid() {
+    if (this.name.length <= 1) {
+      this.isValidName = false;
+    } else this.isValidName = true;
+  }
+
+  registerUser() {
+    this.authService.registerUser(this.name, this.username, this.email, this.password)
+      .subscribe(
+        (response) => {
+          this.token = response.token;
+          this.registrationResponse = response;
+          console.log('Registration successful:', response);
+          localStorage.setItem('token', this.token);
+        },
+        (error) => {
+          console.error('Registration failed:', error);
+        }
+      );
   }
 
   signUpClick(): void {
-    if (this.username == '' || this.password == '' || this.email == '') {
+    if (this.username == '') {
       this.isValidUsername = false
-      this.isValidPassword = false
+      this.isValidForm = false
+    } if (this.name == '') {
+      this.isValidName = false
+      this.isValidForm = false
+    } if (this.email == '') {
       this.isValidEmail = false
-      alert('Please enter a valid data');
+      this.isValidForm = false
+    } if (this.password == '') {
+      this.isValidPassword = false
+      this.isValidForm = false
+    }
+    if (this.isValidForm) {
+      this.registerUser()
+    }
+    else {
+      alert("Please enter a valid data");
     }
   }
 }
