6a70bcf4532fde2e7bb1f8e6b394c5ccce70ffe1
GlebHal <hammanov.gleb@gmail.com>
2024-06-30 10:21:58 +0300

    Unit tests WIP
    - Added unit tests for task services and repostitory
    - Reworks of old tests

---
+++ backend/src/test/java/com/task_management_service/backend/service/TaskServiceTest.java
@@ -0,0 +1,107 @@
+package com.task_management_service.backend.service;
+
+import com.task_management_service.backend.dto.request.CreateTaskDto;
+import com.task_management_service.backend.dto.request.RemoveTaskDto;
+import com.task_management_service.backend.dto.request.UpdateTaskDto;
+import com.task_management_service.backend.entity.TaskEntity;
+import com.task_management_service.backend.entity.UserEntity;
+import com.task_management_service.backend.helper.TaskServiceHelper;
+import com.task_management_service.backend.repository.TaskRepository;
+import com.task_management_service.backend.test_utils.TestValues;
+import com.task_management_service.backend.utils.Converter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TaskServiceTest {
+    @Mock
+    private TaskRepository taskRepository;
+    @Mock
+    private UserService userService;
+    @Mock
+    private TaskServiceHelper taskServiceHelper;
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp() {
+        taskService = new TaskService(taskRepository, userService, taskServiceHelper);
+    }
+
+    @Test
+    void shouldCreateAndReturnTask() {
+        UserEntity testUser = TestValues.getUser();
+        TaskEntity testTask = TestValues.getTask();
+        CreateTaskDto createTaskDto = new CreateTaskDto(
+                testTask.getTitle(),
+                testTask.getDescription(),
+                Converter.convertToIso8601(testTask.getUntilDate()),
+                testTask.getStatus()
+        );
+
+        when(userService.getCurrentUser()).thenReturn(testUser);
+
+        TaskEntity createdTask = taskService.create(createTaskDto);
+
+        assertThat(createdTask).isNotNull();
+        assertThat(createdTask.getTitle()).isEqualTo(testTask.getTitle());
+        assertThat(createdTask.getStatus()).isEqualTo(testTask.getStatus());
+        assertThat(createdTask.getUser().getName()).isEqualTo(testUser.getName());
+    }
+
+    @Test
+    void shouldGetAllTasks() {
+        UserEntity testUser = TestValues.getUser();
+        testUser.getTasks().add(TestValues.getTask(1));
+        testUser.getTasks().add(TestValues.getTask(2));
+        testUser.getTasks().add(TestValues.getTask(3));
+
+        when(userService.getCurrentUser()).thenReturn(testUser);
+
+        ArrayList<TaskEntity> tasks = taskService.getAll();
+
+        assertThat(tasks).isNotNull();
+        assertThat(tasks.size()).isEqualTo(3);
+    }
+
+    @Test
+    void shouldRemoveTask() throws Exception {
+        TaskEntity taskToRemove = TestValues.getTask(1);
+        RemoveTaskDto removeTaskDto = new RemoveTaskDto(
+                UUID.randomUUID().toString()
+        );
+
+        when(taskServiceHelper.findTaskAndMakeUserVerification(any(UUID.class))).thenReturn(taskToRemove);
+
+        assertAll(() -> taskService.remove(removeTaskDto));
+    }
+
+    @Test
+    void shouldUpdateAndReturn() {
+        TaskEntity taskToUpdate = TestValues.getTask(1);
+        String updatedTitle = "Hello world";
+        UpdateTaskDto updateTaskDto = new UpdateTaskDto(
+                UUID.randomUUID().toString(),
+                updatedTitle,
+                taskToUpdate.getDescription(),
+                Converter.convertToIso8601(taskToUpdate.getUntilDate()),
+                taskToUpdate.getStatus()
+        );
+
+        when(taskServiceHelper.findTaskAndMakeUserVerification(any(UUID.class))).thenReturn(taskToUpdate);
+
+        TaskEntity updatedTask = taskService.update(updateTaskDto);
+
+        assertThat(updatedTask.getTitle()).isEqualTo(updatedTitle);
+    }
+}
--- backend/src/test/java/com/task_management_service/backend/test_utils/TestValues.java
+++ backend/src/test/java/com/task_management_service/backend/test_utils/TestValues.java
@@ -1,34 +1,90 @@
 package com.task_management_service.backend.test_utils;
 
+import com.task_management_service.backend.entity.TaskEntity;
 import com.task_management_service.backend.entity.UserEntity;
 import com.task_management_service.backend.enumeration.Role;
+import com.task_management_service.backend.enumeration.TaskStatus;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
 
 public class TestValues {
     public static String getHash() {
         return "$2y$10$R6sdrACYVHHKppthXWDdRuCtLZ5/UTETc51bUfFySEYQrmZ.QzuhO";
     }
 
-    public static UserEntity getUser(String passwordHash) {
+    public static UserEntity getUser(int counter, String passwordHash) {
         return new UserEntity(
-                "Mike",
-                "Mike1902",
-                "mike1999@gmail.com",
+                "Mike" + counter,
+                "MikeUsername" + counter,
+                "mike" + counter + "@gmail.com",
                 passwordHash,
                 Role.USER
         );
     }
 
-    public static UserEntity getUser() {
-        return new UserEntity(
-                "Mike",
-                "Mike1902",
-                "mike1999@gmail.com",
-                "PasswordHash1234",
-                Role.USER
-        );
+    public static UserEntity getUser(int counter) {
+        return getUser(counter, "PasswordHash" + counter);
     }
 
-    public static String getToken() {
+    public static UserEntity getUser() {
+        return getUser(1);
+    }
+
+    public static TaskEntity getTask(int counter, LocalDateTime localDateTime, TaskStatus status) {
+        TaskEntity task = new TaskEntity();
+        task.setTitle("Task" + counter);
+        task.setDescription("Task" + counter + " desc");
+        task.setUntilDate(localDateTime);
+        task.setStatus(status);
+
+        return task;
+    }
+
+    public static TaskEntity getTask(int counter, LocalDateTime localDateTime) {
+        return getTask(counter, localDateTime, TaskStatus.TODO);
+    }
+
+    public static TaskEntity getTask(int counter) {
+        return getTask(counter, LocalDateTime.now(), TaskStatus.TODO);
+    }
+
+    public static TaskEntity getTask() {
+        return getTask(1, LocalDateTime.now(), TaskStatus.TODO);
+    }
+
+    public static String getSigningToken() {
         return "6d653c555a2726326d3b2b2178766c7377753f654e414b525775522331";
+    }
+    public static Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(getSigningToken());
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public static String getJwtToken(Key signingKey, UserDetails userDetails, Date expiration) {
+        return Jwts
+                .builder()
+                .setClaims(new HashMap<>())
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(expiration)
+                .signWith(signingKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public static String getJwtToken(Date expiration) {
+        return getJwtToken(getSigningKey(), getUser(), expiration);
+    }
+
+    public static String getJwtToken() {
+        return getJwtToken(getSigningKey(), getUser(), new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24));
     }
 }
